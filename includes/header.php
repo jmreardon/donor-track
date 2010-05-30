bf15d11d8d036030611c31deefce3788c01ef393
Justin Reardon <jmreardon@jmreardon.com>
2010-05-30 10:56:45 -0400

    add copyright headers to all files

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,19 @@
 <?php
+
+//   Copyright 2008 johnboyproductions.com
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 $query_history = "SELECT * FROM history WHERE history_status = 1 ORDER BY history_date DESC LIMIT 0, 4";
 $history = mysql_query($query_history) or die(mysql_error());
 $row_history = mysql_fetch_assoc($history);
